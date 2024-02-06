83883f28f3c6aedab22e37296f80339540f86282
22sook00 <22sook00@gmail.com>
2024-02-06 21:31:23 +0900

    ♻️ Update: Button component in Common directory

---
+++ src/Common/Button.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { useState } from "react";
+
+const Button = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <div className="flex gap-4 my-4">
+        <button
+          onClick={() => setCount(count + 1)}
+          className="rounded-md px-4 py-2 text-white font-semibold bg-[#334155]"
+        >
+          COMMON BUTTON
+        </button>
+        <button
+          onClick={() => setCount(0)}
+          className="bg-[#f24a6f] text-white rounded-md px-4 py-2 font-semibold"
+        >
+          RESET
+        </button>
+      </div>
+      <p className="font-semibold"> ➕ COUNT: {count}</p>
+    </div>
+  );
+};
+
+export default Button;
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,10 @@
-"use client";
-import { useState } from "react";
+import Button from "@/Common/Button";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
   return (
     <main className="flex border border-sky-100 flex-col items-center  p-24">
       <h3 className="text-2xl text-yellow-400 font-bold">SUB 1</h3>
-      <div className="flex gap-4 my-4">
-        <button
-          onClick={() => setCount(count + 1)}
-          className="rounded-md px-4 py-2 text-white font-semibold bg-[#334155]"
-        >
-          COMMON BUTTON
-        </button>
-        <button
-          onClick={() => setCount(0)}
-          className="bg-[#f24a6f] text-white rounded-md px-4 py-2 font-semibold"
-        >
-          RESET
-        </button>
-      </div>
-      <p className="font-semibold"> ➕ COUNT: {count}</p>
+      <Button />
     </main>
   );
 }
