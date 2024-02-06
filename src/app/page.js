082e6707cfb68f6ebbb14fb8c25ef277a1805d3b
22sook00 <22sook00@gmail.com>
2024-02-06 23:29:17 +0900

    ♻️ Update: simple change text

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Button from "@/Common/Button";
 export default function Home() {
   return (
     <main className="flex border border-sky-100 flex-col items-center  p-24">
-      <h3 className="text-2xl text-yellow-400 font-bold">SUB 1</h3>
+      <h3 className="text-2xl text-yellow-400 font-bold">SUB 1 SUB 1</h3>
       <Button />
     </main>
   );
